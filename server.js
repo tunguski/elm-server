b0b33345636796775b7a7f335cfd94af9c289b36
Marek Romanowski <marek.romanowski@gmail.com>
2016-07-20 01:02:26 +0200

    Simple live reload

--- server.js
+++ server.js
@@ -1,36 +1,48 @@
 const http = require('http');
-const elm = require('./main');
 const static = require('node-static');
+const watch = require('watch')
+
 
 global.XMLHttpRequest = require("xhr2");
 
 
-var app = elm.Example.worker();
+var app;
+
 
 // TODO: switch to redis
 var requestCache = {};
 
 
-app.ports.sendResponse.subscribe(function(response) {
-  var reqRes = requestCache[response.idRequest];
-  if (reqRes) {
-    response.headers.forEach(function (header) {
-      reqRes.res.setHeader(header[0], header[1]);
-    });
-    reqRes.res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8000');
-
-    if (response.statusCode) {
-      reqRes.res.statusCode = response.statusCode;
+function startElmServer() {
+  delete require.cache[require.resolve('./main')]
+  var elm = require('./main');
+  app = elm.Example.worker();
+  
+  
+  app.ports.sendResponse.subscribe(function(response) {
+    var reqRes = requestCache[response.idRequest];
+    if (reqRes) {
+      response.headers.forEach(function (header) {
+        reqRes.res.setHeader(header[0], header[1]);
+      });
+      reqRes.res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8000');
+  
+      if (response.statusCode) {
+        reqRes.res.statusCode = response.statusCode;
+      }
+      if (response.body) {
+        reqRes.res.write(response.body);
+      }
+      reqRes.res.end();
+      requestCache[response.idRequest] = undefined;
+  
+      console.log('' + new Date().getTime() - reqRes.startTime + 'ms: ' + reqRes.req.url);
     }
-    if (response.body) {
-      reqRes.res.write(response.body);
-    }
-    reqRes.res.end();
-    requestCache[response.idRequest] = undefined;
+  });
+}
 
-    console.log('' + new Date().getTime() - reqRes.startTime + 'ms: ' + reqRes.req.url);
-  }
-});
+
+startElmServer();
 
 
 const server = http.createServer((req, res) => {
@@ -58,14 +70,73 @@ const server = http.createServer((req, res) => {
 
 
 const file = new static.Server('./public');
- 
+
+
+function elmBuilder (paths, cmd, postBuild) {
+  var builder = {
+    changed : false
+  };
+
+  
+  var exec;
+
+
+  function maybeBuild () {
+    if (builder.changed && !exec) {
+      exec = require('child_process').exec;
+      builder.changed = false;
+      
+      console.log('Starting build ' + paths);
+      exec(cmd, function(error, stdout, stderr) {
+        exec = undefined;
+
+        console.log(stdout);
+        console.log(stderr);
+        if (postBuild) {
+          postBuild();
+        }
+        if (builder.changed) {
+          maybeBuild();
+        }
+      });
+    }
+  }
+
+
+  paths.forEach(function (path) {
+    watch.watchTree(path, { ignoreDotFiles : true }, function (f, curr, prev) {
+      if (typeof f == "object" && prev === null && curr === null) {
+        // Finished walking the tree
+      } else if (prev === null) {
+        // f is a new file
+        builder.changed = true;
+      } else if (curr.nlink === 0) {
+        // f was removed
+        builder.changed = true;
+      } else {
+        // f was changed
+        builder.changed = true;
+      }
+  
+      maybeBuild();
+    });
+  });
+
+  return builder;
+}
+
+
+const clientBuilder = elmBuilder(['client/'],
+    'elm-make client/Main.elm     --output public/example.js');
+const serverBuilder = elmBuilder(['src/', 'example/'],
+    'elm-make example/Example.elm --output main.js',
+    startElmServer);
+
+
 http.createServer(function (request, response) {
-    request.addListener('end', function () {
-        // 
-        // Serve files! 
-        // 
-        file.serve(request, response);
-    }).resume();
+  request.addListener('end', function () {
+    file.serve(request, response);
+  }).resume();
 }).listen(8000);
 
 
