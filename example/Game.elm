3ad32d23fee293c2048dd51872ec6f2f7a0f0c6d
Marek Romanowski <marek.romanowski@gmail.com>
2017-01-06 17:23:06 +0100

    Skeleton for game play functions

--- example/Game.elm
+++ example/Game.elm
@@ -36,9 +36,74 @@ gamesApiPart api =
                         _ ->
                             statusResponse 405 |> Result
                 )
+            , PF "declareTichu" (\() -> declareTichu api id)
+            , PF "declareGrandTichu" (\() -> declareGrandTichu api id)
+            , PF "pass" (\() -> pass api id)
+            , PF "hand" (\() -> hand api id)
+--            , PF "" (\() ->  api id)
+--            , PF "" (\() ->  api id)
             ]
           )
         ]
+
+
+{-| Pass player's move
+
+1. Check is it actual player
+2. Check if player may pass - there is no requirement or player does not have that card
+3. Update actual player
+4. Save state
+
+-}
+pass api id =
+    doWithTable api id (\session table ->
+        statusResponse 500 |> Task.succeed
+    )
+
+
+{-| Place a hand on table
+
+1. Check if it is actual player or played a bomb (higher if there was bomb before)
+2. Place hand on table
+3. Remove cards from player's hand
+4. Update actual player
+5. Save state
+
+-}
+hand api id =
+    doWithTable api id (\session table ->
+        statusResponse 500 |> Task.succeed
+    )
+
+
+{-| Declare tichu
+
+1. Check if player may declare tichu - did not put any card in this round
+2. Modify state
+
+-}
+declareTichu api id =
+    doWithTable api id (\session table ->
+        statusResponse 500 |> Task.succeed
+    )
+
+
+{-| Declare grand tichu
+
+1. Check if player may declare grand - saw only 8 cards
+2. Modify state
+
+-}
+declareGrandTichu api id =
+    doWithTable api id (\session table ->
+        statusResponse 500 |> Task.succeed
+    )
+
+
+doWithTable api id function =
+    api.doWithSession (\session ->
+        get id games |> andThen (function session)
+    )
 
 
 {-| Return awaiting table with id.
