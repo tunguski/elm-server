c932a54586c57abafac2cca12a481a58ea53701a
Marek Romanowski <marek.romanowski@gmail.com>
2017-01-30 01:21:48 +0100

    Fix constant name generation

--- src/RandomTask.elm
+++ src/RandomTask.elm
@@ -24,15 +24,15 @@ randomIdentifier =
             Random.step
                 (Random.list 8 <| Random.int 0 15)
                 (Random.initialSeed <| floor seed)
-            |> (\result ->
-                Tuple.first result
+            |> (\(result, seed) ->
+                result
                 |> List.map (\i ->
                     if i < 10 then
                         toString i
                     else
                         "abcdef"
                         |> String.dropLeft (i - 10)
-                        |> String.dropRight (i - 10 + 1)
+                        |> String.dropRight (15 - i)
                 )
                 |> String.join ""
             )
